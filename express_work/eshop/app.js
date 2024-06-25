395e919b3543162ae573a657e6215b8d6fa553e1
pawan <meanstack4@gmail.com>
2024-06-25 18:25:20 +0530

    mongo command update

--- express_work/eshop/app.js
+++ express_work/eshop/app.js
@@ -1,11 +1,18 @@
 import express from "express";
 import IndexRouter from "./routes/index.route.js";
+import mongoose from "mongoose";
 const app = express();
 
 app.set("view engine","ejs");
 app.use(express.static("./public"));
 
 // http://localhost:3000/about
+mongoose.connect('mongodb://localhost:27017/userdb')
+.then(result=>{
+    console.log("MongoDB Connected....");
+}).catch(err=>{
+    console.log(err);
+})
 app.use("/",IndexRouter);
 
 app.listen(3000,()=>{
