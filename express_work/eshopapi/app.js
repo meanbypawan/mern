e534c2b2296ba83d2443c6e6d802cf7c80bd5489
pawan <meanstack4@gmail.com>
2024-07-01 18:22:11 +0530

    rest api update 2

--- express_work/eshopapi/app.js
+++ express_work/eshopapi/app.js
@@ -2,13 +2,14 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import UserRouter from "./routes/user.routes.js";
-
+import CategoryRouter from "./routes/category.routes.js";
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 mongoose.connect("mongodb://localhost:27017/eshopdb")
 .then(()=>{
     app.use("/user",UserRouter);
+    app.use("/category",CategoryRouter);
     app.listen(3000,()=>{
         console.log("Server Started");
     });
--- express_work/eshopapi/controller/user.controller.js
+++ express_work/eshopapi/controller/user.controller.js
@@ -1,13 +1,20 @@
 import { User } from "../model/user.model.js";
 import { validationResult } from "express-validator";
-export const signIn = (request,response,next)=>{
-    User.findOne({email: request.body.email,password: request.body.password})
-    .then(result=>{
-        return result ? response.status(200).json({message: "Sign in success"}) : response.status(401).json({error: "Bad request.."});
-    })
-    .catch(err=>{
-        return response.status(500).json({error: "Internal Server Error"});
-    });
+import bcrypt from "bcryptjs";
+export const signIn = async (request,response,next)=>{
+   let {email,password} = request.body;
+   try{
+      let user = await User.findOne({email});
+      if(user){
+        let status = bcrypt.compareSync(password,user.password);
+        user.password = undefined;
+        return status ? response.status(200).json({message: 'Sign in success', user}) : response.status(401).json({error: "Bad request | Unauthorized user"});
+      }  
+      return response.status(401).json({error: "Bad request|Unauthroized user"});
+   }
+   catch(err){
+    return response.status(500).json({error: "Internal Server Error"});
+   } 
 }
 export const signUp = async (request,response,next)=>{
   try{   
@@ -16,6 +23,9 @@ export const signUp = async (request,response,next)=>{
       return response.status(401).json({error: "Bad request",errors: errors.array()});
 
     let {email,password,contact} = request.body;
+    let saltKey = bcrypt.genSaltSync(10);
+    password = bcrypt.hashSync(password,saltKey);
+    
     let user = await User.create({email,password,contact});  
     return response.status(201).json({message: "user saved",user});
   }
